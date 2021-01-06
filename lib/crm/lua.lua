302e9813f496538735aedcbacce83bb50c5bafea
Evan DiBona <evandibona@gmx.com>
2021-01-06 13:48:29 -0500

    Most basics are there now.

--- lib/crm/lua.lua
+++ lib/crm/lua.lua
@@ -11,13 +11,13 @@ function crm.extract( a )
     e.addr     = a - 1
     e.next     = strToInt( string.sub( crm.db,   a, a+3 ) )
     e.date     = strToInt( string.sub( crm.db, a+4, a+7 ) )
-    e.isTrunk  = (  e['next'] >= 2^31  )
+    e.isTrunk  = (  e.next >= 2^31  )
     if e.isTrunk then
-      e.next   = e['next'] - 2^31
-      e.label  = string.sub( crm.db, a+8, e['next']  )
+      e.next   = e.next - 2^31
+      e.label  = string.sub( crm.db, a+8, e.next  )
     else
       e.parent = strToInt( string.sub( crm.db, a+8, a+11) ) 
-      e.data   = string.sub( crm.db, a+12, e['next'] )
+      e.data   = string.sub( crm.db, a+12, e.next )
     end
     return e
   else
@@ -34,90 +34,91 @@ function crm.forEachEntry( f )
   end
 end
 
-function crm.forEachNode( d, f )
-  local entry = {['next']=0}
-  while entry['next'] < #d-1 do
-    entry = pullEntry( d, entry['next'] )
-    if entry['node'] then
-      f( entry )
-    end
+function crm.forRevEntry( f )
+  local es = crm.entries()
+  for i = #es, 1, -1 do
+    f( crm.extract(es[i]) )
   end
 end
 
-function crm.forEachLeaf( d, f )
-  local entry = {['next']=0}
-  while entry['next'] < #d-1 do
-    entry = pullEntry( d, entry['next'] )
-    if not entry['node'] then
-      f( entry )
-    end
+function crm.forEachChildOf( node, f )
+  local children = crm.childrenOf( node )
+  for i=1,#children do
+    f(crm.extract(children[i]))
   end
 end
 
-function crm.addN( data, node )
-  local i = #data
-  local nextA = 2^31 + i
-        nextA = nextA + 8 + #node[2]
-
-  data = data..intToStr( nextA )      -- Next
-  data = data..intToStr( node[1] )    -- Date / Time
-  data = data..node[2]
-
-  return data
+function crm.addT( ref, t )
+  t = t or os.time()
+  local nxt = 2^31 + #crm.db
+        nxt = nxt + 8 + #ref
+  crm.db = crm.db..intToStr( nxt )
+  crm.db = crm.db..intToStr(  t  )
+  crm.db = crm.db..ref
 end
 
-function crm.addL( data, leaf )
-  local i = #data
-  local nextA = i + 12 + #leaf[3]
-  data = data..intToStr( nextA )    --Next
-  data = data..intToStr( leaf[1] )  --Date
-  data = data..intToStr( leaf[2] )  --Node
-  data = data..leaf[3]              --Data
-  return data
+function crm.addL( par, dat, t )
+  t = t or os.time()
+  local nxt = #crm.db + 12 + #dat
+  crm.db = crm.db..intToStr( nxt )
+  crm.db = crm.db..intToStr(  t  )
+  crm.db = crm.db..intToStr( par )
+  crm.db = crm.db..dat
 end
 
-function crm.locN( data, label )
-  local loc = nil
-  crm.forEachNode( data, 
-    function( entry )
-      if entry['label'] == label then
-        loc = entry['loc']
+local function looseMatch( a, b )
+  b = string.sub(b, 1, #a)
+  return a == b
+end
+
+function crm.addr( s )
+  local adr = nil
+  crm.forEachEntry(
+    function( e )
+      if looseMatch(s, (e.label or e.data)) then
+        adr = e.addr
       end
     end
   )
-  return loc
+  return adr
 end
 
-function crm.printChildren( d, node )
-  local nLoc = crm.locN(d, node )
-  print( "#"..node )
+---- Array Returns ----
 
-  crm.forEachLeaf(d, 
-    function( entry )
-      if nLoc == entry['parent'] then
-        print('  * '..entry['data'])
+function crm.entries()
+  local es = {}
+  crm.forEachEntry(
+    function( e )
+      table.insert(es, e.addr)
+    end
+  )
+  return es
+end
+
+function crm.indexTrunks()
+  local ts = {}
+  crm.forEachEntry(
+    function( e )
+      if e.isTrunk then
+        ts[e.label] = e.addr
       end
     end
   )
+  return ts
 end
 
-
-function crm.slurp( l )
-  local f  = io.open( l )
-  local db = ""
-  if f then
-    db = f:read("*a")
-    f:close()
-  else
-    print("\tFile does not exist.")
-  end
-  return db
-end
-
-function crm.save( n, d )
-  local f = io.open( n, 'w' )
-  f:write(d)
-  f:close()
+function crm.childrenOf( node )
+  local ch = {}
+  crm.forEachEntry(
+    function( e )
+      if not e.isTrunk then
+        if node == e.parent then
+          ch[#ch+1] = e.addr
+        end
+      end
+    end
+  )
+  return ch
 end
 
 return crm
--- test-run.lua
+++ test-run.lua
@@ -1,16 +1,32 @@
 crm = require("./lib/crm.lua")
 
-crm.db = crm.slurp('data.db')
+crm.db = ""
 
-crm.forEachEntry(
-  function(entry)
-    if entry.isTrunk then
-      print(entry.label)
-    else
-      print(entry.data)
-    end
+crm.addT("bob")
+crm.addT("josh")
+  crm.addL(crm.addr("josh"), "note:Who is this guy?")
+  crm.addL(crm.addr("josh"), "address: 666 Rainbows End, Suffolk, MO")
+  crm.addL(crm.addr("josh"), "phone: 8072845655")
+crm.addT("luke")
+crm.addT("mark")
+  crm.addL(crm.addr("mark"), "note:Maybe a crazy person?")
+crm.addT("craig")
+  crm.addL(crm.addr("craig"), "note:He just found his way here.")
+
+local index = crm.indexTrunks()
+
+crm.forEachEntry( 
+  function( e )
+    print(e.addr)
   end
 )
+
+crm.forRevEntry( 
+  function( e )
+    print(e.addr)
+  end
+)
+
 
 
 
