6e433b8f66d2980f6b402d0820c1f74faacc2f74
Evan DiBona <evandibona@gmx.com>
2021-01-07 23:00:09 -0500

    Importer seems to work just fine. Any other features will go into the client.

--- lib/csv/lua.lua
+++ lib/csv/lua.lua
@@ -1,14 +1,22 @@
 local csv = {}
 
-function csv.processLine( line )
-  local state = 0
+local function trimLine( l )
+  if string.byte(l, #l) == 13 then 
+    l = string.sub(l, 1, #l-1)  
+  end
+  return l
+end
+
+local function processLine( line )
+  line = trimLine( line )
+  local state = false
   local ary = {}
   local i = 1
   for ix=1,#line do
     local char = string.sub(line,ix,ix)
     if char == '"' then
-      state = 1 - state 
-    elseif (state==0) and (char==',') then
+      state = not state 
+    elseif (not state) and (char==',') then
       i = i + 1
     else
       if ary[i]==nil then ary[i] = "" end
@@ -18,7 +26,7 @@ function csv.processLine( line )
   return ary
 end
 
-function csv.file2Lines( name )
+local function fileToLines( name )
   local lines = {}
   local i = 1
   for line in io.lines(name) do
@@ -35,5 +43,50 @@ function csv.findKey( keys, term )
     end
   end
 end
+
+csv.lines = {}
+csv.keys  = {}
+
+function csv.forEachLineI( f )
+  for i = 1, #csv.lines do
+    f( i, csv.lines[i] )
+  end
+end
+function csv.forEachLine( f )
+  for i = 1, #csv.lines do
+    f( csv.lines[i] )
+  end
+end
+
+function csv.open( n )
+  csv.lines = fileToLines( n )
+  csv.forEachLineI(
+    function( i, v )
+      csv.lines[i] = processLine( v )
+    end
+  )
+  csv.keys = csv.lines[1]
+  table.remove(csv.lines, 1)
+end
+
+function csv.allByKey( f )
+  csv.forEachLineI( function( iy, line )
+    for ix=1,#line do
+      if line[ix] then
+        f( iy, csv.keys[ix], line[ix] )
+      end
+    end
+  end )
+end
+
+function csv.lineByKey( l, f )
+  l = csv.lines[l]
+  for i=1, #l do
+    if l[i] then
+      f( csv.keys[i], l[i] )
+    end
+  end
+end
+
 
 return csv
