668454eab45318a561487b1484b41db30c101cc3
Evan DiBona <evandibona@gmx.com>
2022-07-09 18:31:31 -0400

    Nor do I need these.

--- external/exporter/craigslist-books.lua
+++
@@ -1,4 +0,0 @@
---[[
-  The idea here is to upload the pictures manually, 
-  and then generate the table, description, and other info. 
---]]
--- external/exporter/evansbookshelf.lua
+++
@@ -1,8 +0,0 @@
---[[
-
-  Generate the html part of this website that displays my inventory
-  Take a leaf from truthbleedsred. Possibly come from the same aesthetic
-  Just with new colors. 
-
-
---]]
